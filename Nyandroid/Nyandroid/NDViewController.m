8e2c0729e64b20878cfd3de66dd7c095c24bd4a2
James Power <heathen79@gmail.com>
2013-05-26 10:10:53 -0400

    Added caching of UIImageViews. Should check into whether or not that's really helping/necessary.

--- Nyandroid/Nyandroid/NDViewController.m
+++ Nyandroid/Nyandroid/NDViewController.m
@@ -14,6 +14,7 @@
     CGFloat height, width;
     UIImage* star;
     NSMutableArray* rainbowViews;
+    NSMutableArray* reusableImageViews;
 }
 
 - (UIImage*) imageByName:(NSString*)name {
@@ -24,6 +25,8 @@
     [super viewDidLoad];
     height = [[UIScreen mainScreen] bounds].size.height;
     width = [[UIScreen mainScreen] bounds].size.width;
+    reusableImageViews = [NSMutableArray array];
+    rainbowViews = [NSMutableArray array];
     
     // setup nyandroid
     self.nyandroid.image = [self imageByName:@"nyandroid"];
@@ -50,38 +53,66 @@
     // setup starfield
     star = [self imageByName:@"star"];
     [NSTimer scheduledTimerWithTimeInterval:1.0/7.0 target:self selector:@selector(updateDisplay) userInfo:nil repeats:YES];
+    //[NSTimer scheduledTimerWithTimeInterval:5 target:self selector:@selector(checkStats) userInfo:nil repeats:YES];
 }
 
+- (void) checkStats {
+    NSLog(@"Stored IVs: %d", reusableImageViews.count);
+}
 
 - (void) updateDisplay {
 
     int size;
     int speed;
-    int type = arc4random() % 6;
+    int type = arc4random() % 12;
 
     switch (type) {
         case 0:
+        case 1:
             size = 32;
             speed = 2;
             break;
-        case 1:
+        case 2:
+        case 3:
             size = 24;
             speed = 3;
             break;
-        case 2:
+        case 4:
+        case 5:
             size = 16;
             speed = 3;
             break;
-        default:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+        case 10:
             size = 8;
             speed = 5;
             break;
+        default:
+            return;
+            break;
     }
     
-    OLImageView* iv = [[OLImageView alloc] initWithFrame:CGRectMake(width + star.size.width,
-                                                                    arc4random() % ((int)height),
-                                                                    size,
-                                                                    size)];
+    OLImageView* iv;
+
+    if (reusableImageViews.count > 0) {
+        iv = [reusableImageViews objectAtIndex:0];
+        [reusableImageViews removeObjectAtIndex:0];
+        iv.frame = CGRectMake(width + star.size.width,
+                              arc4random() % ((int)height),
+                              size,
+                              size);
+    } else {
+        iv = [[OLImageView alloc] initWithFrame:CGRectMake(width + star.size.width,
+                                                           arc4random() % ((int)height),
+                                                           size,
+                                                           size)];
+    }
+
+
+
     [iv setRandomImage:star];
     [self.view addSubview:iv];
 
@@ -91,6 +122,8 @@
         iv.frame = frame;
     } completion:^(BOOL finished) {
         [iv removeFromSuperview];
+        [reusableImageViews addObject:iv];
+        //NSLog(@"Count of reusable views: %@, %d", iv, reusableImageViews.count);
     }];
 
     for (UIView* v in rainbowViews) {
